5c3345c4c7663b51c9f334c0f004708f3149b6fe
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-07-12 19:53:01 +0600

    fetching all data from database

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,46 +1,46 @@
+import { MongoClient, ObjectID } from "mongodb";
 import MeetupDetail from "../../Component/meetups/MeetupDetail";
 
-const MeetUpDetails = ({meetupData}) => {
+const MeetUpDetails = ({ meetupData }) => {
     return (
         <MeetupDetail
             id={meetupData.id}
             title={meetupData.title}
             image={meetupData.image}
-            address="55-d blog, chittagong, bangladesh"
-            description="it's my home town"
+            address={meetupData.address}
+            description={meetupData.description}
         />
     );
 }
 
-export const getStaticPaths = async() => {
+export const getStaticPaths = async () => {
+    const client = await MongoClient.connect(`mongodb+srv://myTodos:rafi1234@cluster0.fltsf.mongodb.net/meetups?retryWrites=true&w=majority`)
+    const db = client.db()
+    const meetupCollection = db.collection('meetups')
+
+    const meetups = await meetupCollection.find({}, { _id: 1 }).toArray()
     return {
-        fallback:false,
-        paths: [
-            {
-                params: {
-                    meetupId: 'm1'
-                }
-            },
-            {
-                params: {
-                    meetupId: 'm2'
-                }
-            },
-        ]
+        fallback: false,
+        paths: meetups.map(meetup => ({ params: { meetupId: meetup._id.toString() } }))
     }
 }
 
 export const getStaticProps = async (context) => {
     const meetupId = context.params.meetupId
-    console.log(meetupId)
+
+    const client = await MongoClient.connect(`mongodb+srv://myTodos:rafi1234@cluster0.fltsf.mongodb.net/meetups?retryWrites=true&w=majority`)
+    const db = client.db()
+    const meetupCollection = db.collection('meetups')
+
+    const selectedMeetup = await meetupCollection.findOne({ _id: ObjectID(meetupId)})
     return {
         props: {
             meetupData: {
-                id: meetupId,
-                title: 'this is home',
-                image: "https://www.fairobserver.com/wp-content/uploads/2014/05/shutterstock_163182242.jpg",
-                address: "55-d blog, chittagong, bangladesh",
-                description: "it's my home town"
+                id: selectedMeetup._id.toString(),
+                title: selectedMeetup.title,
+                address: selectedMeetup.address,
+                description: selectedMeetup.description,
+                image: selectedMeetup.image
             }
         }
     }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,22 @@
+import { MongoClient } from 'mongodb'
 import Head from 'next/head'
 import Image from 'next/image'
 import MeetupList from '../Component/meetups/MeetupList'
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'this is home',
-    image: "https://www.fairobserver.com/wp-content/uploads/2014/05/shutterstock_163182242.jpg",
-    description: "it's my home town"
-  },
-  {
-    id: 'm2',
-    title: 'this is home2',
-    image: "https://www.fairobserver.com/wp-content/uploads/2014/05/shutterstock_163182242.jpg",
-    description: "it's my home town in town side"
-  },
-]
+// const DUMMY_DATA = [
+//   {
+//     id: 'm1',
+//     title: 'this is home',
+//     image: "https://www.fairobserver.com/wp-content/uploads/2014/05/shutterstock_163182242.jpg",
+//     description: "it's my home town"
+//   },
+//   {
+//     id: 'm2',
+//     title: 'this is home2',
+//     image: "https://www.fairobserver.com/wp-content/uploads/2014/05/shutterstock_163182242.jpg",
+//     description: "it's my home town in town side"
+//   },
+// ]
 
 export default function Home(props) {  
   return (
@@ -24,9 +25,26 @@ export default function Home(props) {
 }
 
 export const getStaticProps = async() => {
+
+  const client = await MongoClient.connect(`mongodb+srv://myTodos:rafi1234@cluster0.fltsf.mongodb.net/meetups?retryWrites=true&w=majority`)
+  const db = client.db()
+  const meetupCollection = db.collection('meetups')
+
+  const meetups = await meetupCollection.find().toArray()
+  
+  client.close()
+
   return {
     props: {
-      meetups: DUMMY_DATA
-    }
+      meetups: meetups.map(meetup => ({
+        title: meetup.title,
+        address:meetup.address,
+        image: meetup.image,
+        description: meetup.description,
+        id: meetup._id.toString(),
+
+      }))
+    },
+    revalidate: 1
   }
 }
